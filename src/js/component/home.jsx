a6021e7b4be4d70af8e3970818265acc84014589
benjalonso <alonsobenjaminlb@gmail.com>
2022-04-19 19:17:45 +0000

    casi listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,63 +1,83 @@
 import React, { useState } from "react";
 
-//include images into your bundle
+//"0 0 20px #FAF1F0"
 //create your first component
 const Home = () => {
 	const [selectedLight, setLight] = useState("");
 
-	function glow() {
-		setLight((selectedLight) => "0 0 20px #FAF1F0");
-	}
-
 	return (
 		<>
 			<div
-				className="trafficBar bg-dark container"
+				className="trafficBar container"
 				style={{
 					width: "30px",
 					height: "80px",
 					marginTop: "10px",
+					background: "black",
 				}}>
 				.
 			</div>
 
 			<div
-				className="lights container bg-dark rounded"
-				style={{ width: "120px", height: "230px" }}>
+				className="lights container"
+				style={{
+					width: "120px",
+					height: "230px",
+					background: "black",
+					borderRadius: "10px",
+				}}>
 				.
 				<div
-					className="green bg-success rounded-circle"
-					onClick={glow}
+					onClick={() => setLight("green")}
+					className={
+						"green" +
+						(selectedLight === "green"
+							? "shadow-sm p-3 mb-5 bg-white rounded"
+							: " ")
+					}
 					style={{
 						width: "60px",
 						height: "60px",
-						boxShadow: selectedLight,
 						margin: "auto",
 						marginBottom: "5px",
+						background: "green",
+						borderRadius: "50%",
 					}}>
 					.
 				</div>
 				<div
-					className="yellow bg-warning rounded-circle"
-					onClick={glow}
+					onClick={() => setLight("yellow")}
+					className={
+						"yellow" +
+						(selectedLight === "yellow"
+							? "shadow-sm p-3 mb-5 bg-white rounded"
+							: " ")
+					}
 					style={{
 						width: "60px",
 						height: "60px",
-						boxShadow: selectedLight,
 						margin: "auto",
 						marginBottom: "5px",
+						background: "yellow",
+						borderRadius: "50%",
 					}}>
 					.
 				</div>
 				<div
-					className="red bg-danger rounded-circle"
-					onClick={glow}
+					onClick={() => setLight("red")}
+					className={
+						"red" +
+						(selectedLight === "red"
+							? "shadow-sm p-3 mb-5 bg-white rounded"
+							: " ")
+					}
 					style={{
 						width: "60px",
 						height: "60px",
-						boxShadow: selectedLight,
 						margin: "auto",
 						marginBottom: "5px",
+						background: "red",
+						borderRadius: "50%",
 					}}>
 					.
 				</div>
